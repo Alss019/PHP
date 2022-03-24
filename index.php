56a6110ba8e19f31ce2b754e54b5bd258b0423ce
Corentin <plaacorentin@gmail.com>
2022-03-24 11:03:02 +0100

    premier

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "hello world";
+    echo '<br>';
+    $int = 5;
+    echo $int;
+    echo '<br>';
+    echo gettype($int);
+    echo '<br>';
+    $string = "Corentin";
+    echo $string;
+    echo '<br>';
+    $bool = false;
+    echo gettype($bool);
+    echo'<br>';
+    $a = 12;
+    $b= 10;
+    $total = $a + $b;
+    echo $total
+?>
+</body>
+</html>
